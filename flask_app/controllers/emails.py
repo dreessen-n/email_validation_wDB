fa26ad049f37d500bb1684bac761e032a08df47f
dreessen-n <dreessen.edu@gmail.com>
2022-07-08 23:14:55 -0600

    Working on routes

--- flask_app/controllers/emails.py
+++ flask_app/controllers/emails.py
@@ -5,7 +5,7 @@ from flask_app import app
 from flask import Flask, render_template, request, redirect, session, url_for
 
 # Import models class
-# Example: from flask_app.models.dojo import Dojo
+from flask_app.models import email
 
 # Create the routes
 
@@ -14,8 +14,26 @@ def index():
     """Homepage"""
     return render_template('index.html')
 
-
 # TODO set routes to CREATE - INSERT into db in models
+@app.route('/create', methods=['POST'])
+def add_email():
+    if not Email.validate_email(request.form):
+        # Redirect back to form
+        return redirect('/')
+    # Create data dict based on request.form
+    # The keys must match exactly to the variables in query str
+    data = {
+        'email': request.form['email'],
+}
+    # Pass the data dict to create_email method in class
+    email.Email.create_email(data)
+    # Redirect to display page
+    return redirect('/success')
+
+
+
 # TODO set routes to READ - SELECT from db in models
+
 # TODO set routes to UPDATE - UPDATE from db in models
+
 # TODO set routes to DELETE - DELETE from db in models
