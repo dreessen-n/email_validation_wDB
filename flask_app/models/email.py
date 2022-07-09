8ac03234cc8ca2ba50a543ce1113f7ec62a54c12
dreessen-n <dreessen.edu@gmail.com>
2022-07-08 22:52:20 -0600

    Added class, static and validation methods

--- flask_app/models/email.py
+++ flask_app/models/email.py
@@ -1,11 +1,15 @@
 # Import mysqlconnection config
 from flask_app.config.mysqlconnection import connectToMySQL
+import re # The regex module
 
 """
 Import other models files for access to classes.
 We import the file rather than the class to avoid circular import
 Example: from flask_app.models import ninja 
 """
+
+# Regular expression for email validation
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 class Email:
     def __init__(self.data):
@@ -15,4 +19,32 @@ class Email:
         self.created_at = data['created_at'],
         self.updated_at = data['updated_at']
 
+    @classmethod
+    def create_email(data):
+        """Add email to db based on info passed from form"""
+        query = "SELECT INTO emails (email) VALUES (%(email)s);"
+        return connectToMySQL('email_val_with_db').query_db(query, data)
+
+    @classmethod
+    def display_emails(cls):
+        """Show all the emails in db"""
+        query = "SELECT * FROM emails;"
+        emails_from_db = connectToMySQL('email_val_with_db').query_db(query)
+        emails = []
+        for em in emails_from_db:
+            emails.append(cls(em))
+        return emails
+
+    @staticmethod
+    def validate_email(email):
+        """Validate that the email is in proper form and exist"""
+        is_valid = True
+        # Test whether a field matches the pattern
+        if not EMAIL_REGEX.match(user['email']):
+            flash("Email is not valid!", "error")
+            is_valid = False
+        return is_valid
+
+
 # TODO Create classmethod to interact with db: CRUD methods
+
